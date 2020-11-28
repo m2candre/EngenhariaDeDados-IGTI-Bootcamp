9a5afccf062ffd62ab35e807d8f541d5c3a2397f
Marcos Andre <m2c.andre@gmail.com>
2020-11-28 10:46:43 -0300

    Iniciando o controle das atividades
    Projetos
      Enade
      Enem
      Twitter

---
+++ Modulo01/Projeto_Twitter/get_tweets.py
@@ -0,0 +1,29 @@
+import json
+from tweepy import OAuthHandler, Stream, StreamListener
+from datetime import datetime
+
+consumer_key = "Lk7l1MmTSwJv03s8Mv5CkRNml"
+consumer_secret = "sLnhiWcxlj5gRApaUSe9wWKRfzN4tbnEsGAGlvu7xmnp57Msr0"
+
+access_token = "1331006111349141508-LdrrMlEpEhUs2Ul9PanVMSPXNfitl8"
+access_token_secret = "MPvwLKlV4Z5MoanxivPpZvpSHPpRbN6hkdKBygp3APxeh"
+
+# Arquivo de saída com os Tweets
+data_hoje = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+out = open(f"collected_tweets{}.txt", "w")
+
+# Conexão com Tweets
+class MyListener(StreamListener):
+
+    def on_data(self, data):
+       print(data)
+       itemString = json.dumps(data)
+       out.write(itemString + "\n")
+       return True
+
+    def on_error(self, status):
+        print(status)
+
+if __name__ == "__main__":
+    l = MyListener()
+    auth = O
