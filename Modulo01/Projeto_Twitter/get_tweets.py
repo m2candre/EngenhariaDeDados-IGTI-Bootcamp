95eb617537b159981c04474d0f6ee13db61ce261
Marcos Andre <m2c.andre@gmail.com>
2020-12-01 10:32:18 -0300

    Movendo as chaves que ficaram expostas

--- Modulo01/Projeto_Twitter/get_tweets.py
+++ Modulo01/Projeto_Twitter/get_tweets.py
@@ -1,27 +1,30 @@
+import config
 import json
 from tweepy import OAuthHandler, Stream, StreamListener
 from datetime import datetime
 
-consumer_key = "Lk7l1MmTSwJv03s8Mv5CkRNml"
-consumer_secret = "sLnhiWcxlj5gRApaUSe9wWKRfzN4tbnEsGAGlvu7xmnp57Msr0"
+consumer_key = config.twitter_api_key
+consumer_secret = config.twitter_api_secret_key
 
-access_token = "1331006111349141508-LdrrMlEpEhUs2Ul9PanVMSPXNfitl8"
-access_token_secret = "MPvwLKlV4Z5MoanxivPpZvpSHPpRbN6hkdKBygp3APxeh"
+access_token = config.twitter_access_token
+access_token_secret = config.twitter_access_token_secret
 
 # Arquivo de saída com os Tweets
 data_hoje = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
 out = open(f"collected_tweets{data_hoje}.txt", "w")
 
+
 # Conexão com Tweets
 class MyListener(StreamListener):
 
     def on_data(self, data):
-       itemString = json.dumps(data)
-       out.write(itemString + "\n")
-       return True
+        itemString = json.dumps(data)
+        out.write(itemString + "\n")
+        return True
 
     def on_error(self, status):
         print(status)
+
 
 if __name__ == "__main__":
     l = MyListener()
